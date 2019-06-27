e6e57f44ce31f079a34b4a70e818c23ef8565bf4
Theodore Thayib <theodore.thayib6512@mb.rctc.edu>
2019-06-27 11:14:01 -0700

    06262019 end of day commit

--- 5D plotting/electrodes_vid.m
+++ 5D plotting/electrodes_vid.m
@@ -4,9 +4,11 @@ number_of_patients = 1;
 number_of_time = 44;
 number_of_electrodes = 72;
 IPthreshold = 0.05;
-frame_rate = 2;
+frame_rate = 10;
 circle_size = 25;
 max_threshold = 0.5;
+
+colormatrix = [];
 
 % light_blue_color = [0.39 0.58 0.93];
 % dark_blue_color = [0 0 .5];
@@ -39,15 +41,18 @@ eLocation = all_loc(patients{1});
 testnum = 0;
 testmatrix = [];
 
-plot_brain;
-view(-90, 0);
+% plot_brain;
+% view(-90, 0);
 
-for pNum = 1:number_of_patients
+for tNum = 1:number_of_time
     eLocation = all_loc(patients{pNum});
     elec_field = getfield(IPtime2(patients{pNum}),'ip_all');
 %     IPvalue = elec_field(:,:,:);
+    plot_brain;
+    view(-90, 0);
+    fprintf("time: " tNum);
     
-    for tNum = 1:number_of_time
+    for pNum = 1:number_of_patients
         figure(1);
         IPvalue_matrix = elec_field(:,1,tNum);
         eSize = size(IPvalue_matrix,1);
@@ -63,7 +68,7 @@ for pNum = 1:number_of_patients
                 testmatrix = [testmatrix;eNum IPvalue pNum];
                 
                 if IPvalue > 0
-                    color_value = max_threshold / IPvalue;
+                    color_value = IPvalue / max_threshold;
                     if color_value > 1
                         color_value = 1;
                     end    
@@ -73,7 +78,7 @@ for pNum = 1:number_of_patients
 
                     c = [red_color blue_color green_color];	
                 else
-                    color_value = -max_threshold / IPvalue;
+                    color_value = IPvalue / -max_threshold;
                     if color_value > 1
                         color_value = 1;
                     end   
@@ -84,6 +89,7 @@ for pNum = 1:number_of_patients
                     c = [red_color blue_color green_color];
                 end
                 
+                colormatrix = [colormatrix;IPvalue c];
                 hold on;
                 s = scatter3(x, y, z, sz, c, 'filled');     
             end
@@ -91,10 +97,69 @@ for pNum = 1:number_of_patients
         
         frame = getframe(gcf);
         writeVideo(v,frame);
+        clf;
     end
     
     
 end
+
+
+
+
+
+% for pNum = 1:number_of_patients
+%     eLocation = all_loc(patients{pNum});
+%     elec_field = getfield(IPtime2(patients{pNum}),'ip_all');
+% %     IPvalue = elec_field(:,:,:);
+%     
+%     for tNum = 1:number_of_time
+%         figure(1);
+%         IPvalue_matrix = elec_field(:,1,tNum);
+%         eSize = size(IPvalue_matrix,1);
+%         
+%         for eNum = 1:eSize
+%             IPvalue = IPvalue_matrix(eNum, 1);
+% 
+%             if (IPvalue_matrix(eNum) >= IPthreshold) || (IPvalue_matrix(eNum) <= -IPthreshold)
+%                 x = eLocation(eNum, 1);
+%                 y = eLocation(eNum, 2);
+%                 z = eLocation(eNum, 3);
+%                 testnum = testnum + 1;
+%                 testmatrix = [testmatrix;eNum IPvalue pNum];
+%                 
+%                 if IPvalue > 0
+%                     color_value = max_threshold / IPvalue;
+%                     if color_value > 1
+%                         color_value = 1;
+%                     end    
+%                     red_color = color_value * -4/9 + 47/45;
+%                     blue_color = color_value * -4/9 + 4/9;
+%                     green_color = color_value * -4/9 + 4/9;
+% 
+%                     c = [red_color blue_color green_color];	
+%                 else
+%                     color_value = -max_threshold / IPvalue;
+%                     if color_value > 1
+%                         color_value = 1;
+%                     end   
+%                     red_color = color_value * -13/30 + 13/30;
+%                     blue_color = color_value * -58/90 + 58/90;
+%                     green_color = color_value * -43/90 + 44/45;
+% 
+%                     c = [red_color blue_color green_color];
+%                 end
+%                 
+%                 hold on;
+%                 s = scatter3(x, y, z, sz, c, 'filled');     
+%             end
+%         end
+%         
+%         frame = getframe(gcf);
+%         writeVideo(v,frame);
+%     end
+%     
+%     
+% end
 
 % for tNum = 1:number_of_time
 %     current_time = tNum;
