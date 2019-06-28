8fe4a6809c05fa63d06f1ccb8dfb4dd96f8cbeb0
Theodore Thayib <theodore.thayib6512@mb.rctc.edu>
2019-06-28 14:38:48 -0700

    electrodes_vid_2 works with all 6 frequencies using tight_subplot. Final commit 06/28/2019

--- 5D plotting/electrodes_vid_2.m
+++ 5D plotting/electrodes_vid_2.m
@@ -1,19 +1,18 @@
 % VARIABLES
 number_of_frequencies = 6;
 number_of_patients = 2;
-number_of_time = 5;
+number_of_time = 44;
 number_of_electrodes = 72;
 IPthreshold = 0.05;
 frame_rate = 2;
 circle_size = 25;
 max_threshold = 0.5;
 
-colormatrix = [];
-
 light_blue_color = [0.52 1 0.99];
 dark_blue_color = [0.14 0 0.34];
 light_red_color = [0.99 0.89 0.01];
 dark_red_color = [0.36 0 0.11];
+fig_bg_color = [0.8 0.8 0.8];
 
 blueRedColorOne = (dark_blue_color(1) - light_blue_color(1)) / 0.9;
 blueRedColorTwo = dark_blue_color(1) - blueRedColorOne;
@@ -29,21 +28,19 @@ redBlueColorTwo = dark_red_color(2) - redBlueColorOne;
 redGreenColorOne = (dark_red_color(3) - light_red_color(3)) / 0.9;
 redGreenColorTwo = dark_red_color(3) - redGreenColorOne;
 
-sz = circle_size;
-
 load('patients.mat');
 load('all_loc.mat');
 load('FINAL_AE2.mat');
 load('IPtime2.mat');
 
-v = VideoWriter('VidExport.avi');
+v = VideoWriter('VidExport2.avi');
 v.FrameRate = frame_rate;
 open(v);
 
 eLocation = all_loc(patients{1});
 
-testnum = 0;
-testmatrix = [];
+figure(1);
+set(gcf,'color',fig_bg_color);
 
 for tNum = 1:number_of_time
     tNum
@@ -55,11 +52,11 @@ for tNum = 1:number_of_time
         elec_rows = 0;
         
         for pNum = 1:number_of_patients
-            pNum
             eLocation = all_loc(patients{pNum});
             elec_field = getfield(IPtime2(patients{pNum}),'ip_all');
 
-            figure(1);
+%             figure(1);
+%             set(gcf,'color',fig_bg_color);
             IPvalue_matrix = elec_field(:,fNum,tNum);
             eSize = size(IPvalue_matrix,1);
 
@@ -70,9 +67,7 @@ for tNum = 1:number_of_time
                             x = eLocation(eNum, 1);
                             y = eLocation(eNum, 2);
                             z = eLocation(eNum, 3);
-                            testnum = testnum + 1;
-                            testmatrix = [testmatrix;eNum IPvalue pNum];
-
+                            
                             if IPvalue > 0
                                 color_value = IPvalue / max_threshold;
                                 if color_value > 1
@@ -94,9 +89,7 @@ for tNum = 1:number_of_time
 
                                 c = [red_color blue_color green_color];
                             end
-
-
-                            patient_matrix = [x y z sz c];
+                            patient_matrix = [x y z circle_size c];
 
                             elec_matrix = [elec_matrix;patient_matrix];
                             elec_rows = elec_rows + 1;
@@ -106,6 +99,7 @@ for tNum = 1:number_of_time
         elec_rows_matrix = [elec_rows_matrix;elec_rows];
     end
     ha = tight_subplot(6, 1, [0.0 0.0], [0.0 0.0], [0.0 0.0]);
+    axis('off');
     fprintf("PLOTTING")
     hold on;
     
@@ -115,17 +109,22 @@ for tNum = 1:number_of_time
            row_for_this_frequency = elec_rows_matrix(1);
            row_start = 1; %what row the subplot should start on
         else
-            row_start = elec_rows_matrix(subplot_num - 1) + 1
-            row_for_this_frequency = row_start - elec_rows_matrix(subplot_num)
+%             row_start = elec_rows_matrix(subplot_num - 1) + 1
+            row_start = row_start + elec_rows_matrix(subplot_num - 1)
+%             row_for_this_frequency = row_start - elec_rows_matrix(subplot_num)
+            row_for_this_frequency = elec_rows_matrix(subplot_num)
         end
+        testvar = row_for_this_frequency + row_start;
         axes(ha(subplot_num)); 
         plot_brain; 
-        for row_number = row_start:(row_for_this_frequency + row_start)
+        for row_number = row_start:(row_for_this_frequency + row_start - 1)
             markercolor = [elec_matrix(row_number,5) elec_matrix(row_number,6) elec_matrix(row_number,7)];
             
             s = scatter3(elec_matrix(row_number,1),elec_matrix(row_number,2),elec_matrix(row_number,3),elec_matrix(row_number,4),markercolor,'filled');
             end
-        end
+    end
+        
+    
 
     fprintf("DONE PLOTTING")
     
