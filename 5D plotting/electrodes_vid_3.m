af104704f81db24bd1fb03c4dc5a20c66bbe16d2
Theodore Thayib <theodore.thayib6512@mb.rctc.edu>
2019-07-25 14:33:30 -0700

    July 25, 2019 Improved code readability

--- 5D plotting/electrodes_vid_3.m
+++ 5D plotting/electrodes_vid_3.m
@@ -1,9 +1,10 @@
 % Code for plotting electrodes onto the brain.
 
 % Requires the following files: patients.mat, all_loc.mat, FINAL_AE2.mat,
-% IPtime2.mat, hemispheres.mat, BRAIN_SCHEME.mat, 
-% fread3.m, plot3_wrapper.m, plotsurf_wrapper.m, read_surf_wrapper.m, 
-% read_surf.m, render_freesurfer3D.m, time_graph.m, and Colorbar_new3.png.
+% IPtime2.mat, hemispheres.mat, BRAIN_SCHEME.mat, fread3.m,
+% plot3_wrapper.m, plotsurf_wrapper.m, read_surf_wrapper.m, read_surf.m,
+% render_freesurfer3D.m, time_graph.m, subtightplot.m, and
+% Colorbar_new3.png.
 
 % VARIABLES
 tic %Tracks how long the code runs for
@@ -27,7 +28,7 @@ vid_name = 'VidExport_FINAL_4_6Freq_5View_44time_139patients.avi';
 
 % Set save_picture to 1 if you want each time-frame to be saved to a png
 % file. Set folder pictures to be saved in to pic_export_folder
-save_picture = 0;
+save_picture = 1;
 pic_export_folder = '5D plotting\pic_exports';
 
 % Frequency legend text.
@@ -42,7 +43,7 @@ font_size = 32; %Frequency legend text font size
 
 
 % Variables for colors, electrodes, and figure background (RGB triplets)
-% Note that the colorbar is a png file (edited through the 
+% Note that the colorbar is a png file (edited through the
 % colorbar_new2.psd photoshop file) - if the colors are changed here, they
 % need to be manually changed within the photoshop file as well (gradient
 % tool).
@@ -93,7 +94,7 @@ open(v);
 f = figure(1);
 set(gcf,'units','normalized','outerposition',[0 0 1 1],'color',fig_bg_color,'WindowState','maximized','InvertHardcopy','off','Renderer','OpenGL')
 
-% EMPTY FRAMES AT BEGINNING OF VIDEO
+% EMPTY FRAMES AT BEGINNING OF VIDEO 
 % Upper time graph
 subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [1 number_of_views + 1], [0.0 0.0],0,0);
 time_graph(0, fig_bg_color, word_on_time, word_off_time);
@@ -107,7 +108,7 @@ subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [(number_of_vie
 
 % Plots empty brain pictures
 for subplot_num = 1:number_of_frequencies
-%     Text for the frequency legend
+    % Text for the frequency legend
     switch subplot_num
         case 1
             frequency_legend = f1;
@@ -122,10 +123,10 @@ for subplot_num = 1:number_of_frequencies
         case 6
             frequency_legend = f6;
     end
-%   Plots brains in the correct views
+    % Plots brains in the correct views
     for current_view = 1: number_of_views
         subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [(number_of_views + 1)* subplot_num * 3 - (number_of_views + 1) * 2 + current_view (number_of_views + 1) * subplot_num * 3 + current_view], [0.0 0.0],0,[0.1 0]);
-
+        
         hold on;
         switch current_view
             case 1
@@ -153,7 +154,7 @@ end
 hold off;
 % Saves number_of_empty_frames frames of the empty brain to the beginning
 % of the video.
-for empty_plot = 1:number_of_empty_frames  
+for empty_plot = 1:number_of_empty_frames
     frame = getframe(gcf);
     writeVideo(v,frame);
 end
@@ -162,93 +163,91 @@ fullFileName = fullfile(pic_export_folder, 'empty_brain.png');
 saveas(gcf,fullFileName);
 
 clf;
- 
-% LOOPS FOR ELECTRODES
+
+% PLOTS ELECTRODES IN BRAINS LOOPS FOR ELECTRODES
 for tNum = 1:number_of_time
     tNum % To see how far along the code is - can be commented out
     
-%     Resets elec_rows_matrix and elec_matrix
+    % Resets elec_rows_matrix and elec_matrix
     elec_rows_matrix = [];
     elec_matrix = [];
     
     for fNum = 1:number_of_frequencies
-%         Resets elec_rows counter
+        % Resets elec_rows counter
         elec_rows = 0;
         
         for pNum = 1:number_of_patients
-%             Gets location and IP values of electrodes of the current
-%             patient
+            % Gets location and IP values of electrodes of the current
+            % patient
             eLocation = all_loc(patients{pNum});
             elec_field = getfield(IPtime2(patients{pNum}),'ip_all');
             
             IPvalue_matrix = elec_field(:,fNum,tNum);
-            eSize = size(IPvalue_matrix,1); %Amount of electrodes in patient
+            eSize = size(IPvalue_matrix,1); % Amount of electrodes in 
+                                            % patient
             eHemisphereFull = hemispheres(patients{pNum});
-
-                    for eNum = 1:eSize
-                        IPvalue = IPvalue_matrix(eNum, 1);
-                        
-%                         Checks to see if the electrode is within the IP
-%                         threhsolds set above. Ignores electrodes that are
-%                         not within the thresholds.
-                        if (IPvalue_matrix(eNum) >= min_ip_threshold) || (IPvalue_matrix(eNum) <= -min_ip_threshold)
-                            x = eLocation(eNum, 1);
-                            y = eLocation(eNum, 2);
-                            z = eLocation(eNum, 3);
-                            
-%                             Colors electrodes respective to their IP
-%                             values
-                            if IPvalue > 0
-                                color_value = IPvalue / max_ip_threshold;
-                                if color_value > 1
-                                    color_value = 1;
-                                end    
-                                red_color = color_value * redRedColorOne + redRedColorTwo;
-                                blue_color = color_value * redBlueColorOne + redBlueColorTwo;
-                                green_color = color_value * redGreenColorOne + redGreenColorTwo;
-                            else
-                                color_value = IPvalue / -max_ip_threshold;
-                                if color_value > 1
-                                    color_value = 1;
-                                end   
-                                red_color = color_value * blueRedColorOne + blueRedColorTwo;
-                                blue_color = color_value * blueBlueColorOne + blueBlueColorTwo;
-                                green_color = color_value * blueGreenColorOne + blueGreenColorTwo;
-                            end
-                            
-                            c = [red_color blue_color green_color];
-%                             Saves what hemispehre the electrodes are in
-                            eHemisphere = eHemisphereFull(eNum);
-                            patient_matrix = [x y z c eHemisphere];
-                            
-%                             Adds all electrode information (x y z
-%                             coordinates, color, and hemisphere) into one
-%                             big matrix (elec_matrix) and adds one to the
-%                             number of rows
-                            elec_matrix = [elec_matrix;patient_matrix];
-                            elec_rows = elec_rows + 1;
+            
+            for eNum = 1:eSize
+                IPvalue = IPvalue_matrix(eNum, 1);
+                
+                % Checks to see if the electrode is within the IP
+                % threhsolds set above. Ignores electrodes that are not
+                % within the thresholds.
+                if (IPvalue_matrix(eNum) >= min_ip_threshold) || (IPvalue_matrix(eNum) <= -min_ip_threshold)
+                    x = eLocation(eNum, 1);
+                    y = eLocation(eNum, 2);
+                    z = eLocation(eNum, 3);
+                    
+                    % Colors electrodes respective to their IP values
+                    if IPvalue > 0
+                        color_value = IPvalue / max_ip_threshold;
+                        if color_value > 1
+                            color_value = 1;
                         end
+                        red_color = color_value * redRedColorOne + redRedColorTwo;
+                        blue_color = color_value * redBlueColorOne + redBlueColorTwo;
+                        green_color = color_value * redGreenColorOne + redGreenColorTwo;
+                    else
+                        color_value = IPvalue / -max_ip_threshold;
+                        if color_value > 1
+                            color_value = 1;
+                        end
+                        red_color = color_value * blueRedColorOne + blueRedColorTwo;
+                        blue_color = color_value * blueBlueColorOne + blueBlueColorTwo;
+                        green_color = color_value * blueGreenColorOne + blueGreenColorTwo;
                     end
+                    
+                    c = [red_color blue_color green_color];
+                    % Saves what hemispehre the electrodes are in
+                    eHemisphere = eHemisphereFull(eNum);
+                    patient_matrix = [x y z c eHemisphere];
+                    
+                    % Adds all electrode information (x y z coordinates,
+                    % color, and hemisphere) into one big matrix
+                    % (elec_matrix) and adds one to the number of rows
+                    elec_matrix = [elec_matrix;patient_matrix];
+                    elec_rows = elec_rows + 1;
+                end
+            end
         end
         elec_rows_matrix = [elec_rows_matrix;elec_rows];
     end
     % Upper time graph
     subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [1 number_of_views + 1], [0.0 0.0],0,0);
     time_graph(0, fig_bg_color, word_on_time, word_off_time);
-
+    
     % Lower time graph
     subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [(number_of_views + 1) * number_of_frequencies * 3 + number_of_views + 2 (number_of_views + 1) * number_of_frequencies * 3 + (number_of_views + 1) * 2], [0.0 0.0],0,0);
     time_graph(0, fig_bg_color, word_on_time, word_off_time);
-
+    
     % Colorbar
     subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [(number_of_views + 1) * 3 (number_of_views + 1) * (number_of_frequencies * 3)], [],[],[0.2 0.02]);    imshow(CBar);
     
     hold on;
     
-%     Brain graphs
-%     subplot_num is for the rows of the plots (1 is the first brain plot,
-%     etc)
-    for subplot_num = 1:number_of_frequencies 
+    % Brain graphs subplot_num is for the rows of the plots (1 is the
+    % first brain plot, etc)
+    for subplot_num = 1:number_of_frequencies
         switch subplot_num
             case 1
                 frequency_legend = f1;
@@ -263,12 +262,12 @@ for tNum = 1:number_of_time
             case 6
                 frequency_legend = f6;
         end
-%         Prepares for figuring out what rows of the matrix each frequency
-%         uses
+        % Prepares for figuring out what rows of the matrix each frequency
+        % uses
         row_count = 1;
         if subplot_num == 1
-           row_for_this_frequency = elec_rows_matrix(1);
-           row_start = 1; %what row the subplot should start on
+            row_for_this_frequency = elec_rows_matrix(1);
+            row_start = 1; % What row the subplot should start on
         else
             row_start = row_start + elec_rows_matrix(subplot_num - 1);
             row_for_this_frequency = elec_rows_matrix(subplot_num);
@@ -277,7 +276,7 @@ for tNum = 1:number_of_time
             subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [(number_of_views + 1)* subplot_num * 3 - (number_of_views + 1) * 2 + current_view (number_of_views + 1) * subplot_num * 3 + current_view], [0.0 0.0],0,[0.1 0]);
             
             hold on;
-%             Plots brains in the correct views
+            % Plots brains in the correct views
             switch current_view
                 case 1
                     plotsurf_wrapper(vL, fL, [0.7, 0.7, 0.7]);
@@ -298,9 +297,9 @@ for tNum = 1:number_of_time
                     view(90,0);
             end
             axis('off');zoom(1);camlight;
-            set(gca,'XLim',[-75 75],'YLim',[-125 100],'ZLim',[-75 100]); 
+            set(gca,'XLim',[-75 75],'YLim',[-125 100],'ZLim',[-75 100]);
             
-%             Plots electrodes (in correct hemisphere)
+            % Plots electrodes (in correct hemisphere)
             if current_view <= 2
                 for row_number = row_start:(row_for_this_frequency + row_start - 1)
                     if elec_matrix(row_number,7) == 1
@@ -308,7 +307,7 @@ for tNum = 1:number_of_time
                         x = elec_matrix(row_number,1);
                         y = elec_matrix(row_number,2);
                         z = elec_matrix(row_number,3);
-
+                        
                         p = plot3(elec_matrix(row_number,1),elec_matrix(row_number,2),elec_matrix(row_number,3),'o','MarkerSize',circle_size,'Color',markercolor,'MarkerFaceColor',markercolor);
                     end
                 end
@@ -319,29 +318,29 @@ for tNum = 1:number_of_time
                         x = elec_matrix(row_number,1);
                         y = elec_matrix(row_number,2);
                         z = elec_matrix(row_number,3);
-
+                        
                         plot3(elec_matrix(row_number,1),elec_matrix(row_number,2),elec_matrix(row_number,3),'o','MarkerSize',circle_size,'Color',markercolor,'MarkerFaceColor',markercolor);
                     end
                 end
-            else        
+            else
                 for row_number = row_start:(row_for_this_frequency + row_start - 1)
                     markercolor = [elec_matrix(row_number,4) elec_matrix(row_number,5) elec_matrix(row_number,6)];
                     x = elec_matrix(row_number,1);
                     y = elec_matrix(row_number,2);
                     z = elec_matrix(row_number,3);
-
+                    
                     plot3(elec_matrix(row_number,1),elec_matrix(row_number,2),elec_matrix(row_number,3),'o','MarkerSize',circle_size,'Color',markercolor,'MarkerFaceColor',markercolor);
                 end
             end
-        end      
+        end
     end
     hold off;
     
-%     Writes current figure to the video
+    % Writes current figure to the video
     frame = getframe(gcf);
     writeVideo(v,frame);
     
-%     If save_picture is on, it will save the figure as a png file
+    % If save_picture is on, it will save the figure as a png file
     if save_picture == 1
         pngFileName = sprintf('time_%d.png', tNum);
         fullFileName = fullfile(pic_export_folder, pngFileName);
@@ -350,7 +349,7 @@ for tNum = 1:number_of_time
     clf;
 end
 
-% EMPTY FRAMES AT THE END OF VIDEO
+% EMPTY FRAMES AT THE END OF VIDEO 
 % Upper time graph
 subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [1 number_of_views + 1], [0.0 0.0],0,0);
 time_graph(0, fig_bg_color, word_on_time, word_off_time);
@@ -364,7 +363,7 @@ subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [(number_of_vie
 imshow(CBar);
 % Plots empty brain pictures.
 for subplot_num = 1:number_of_frequencies
-%     Text for the frequency legend.
+    % Text for the frequency legend.
     switch subplot_num
         case 1
             frequency_legend = f1;
@@ -379,10 +378,10 @@ for subplot_num = 1:number_of_frequencies
         case 6
             frequency_legend = f6;
     end
-%   Plots brains in the correct views
+    % Plots brains in the correct views
     for current_view = 1: number_of_views
         subtightplot(number_of_frequencies * 3 + 2, number_of_views + 1, [(number_of_views + 1)* subplot_num * 3 - (number_of_views + 1) * 2 + current_view (number_of_views + 1) * subplot_num * 3 + current_view], [0.0 0.0],0,[0.1 0]);
-
+        
         hold on;
         switch current_view
             case 1
@@ -408,15 +407,15 @@ for subplot_num = 1:number_of_frequencies
     end
 end
 hold off;
+
 % Saves number_of_empty_frames frames of the empty brain to the end of the
 % video.
-for empty_plot = 1:number_of_empty_frames  
+for empty_plot = 1:number_of_empty_frames
     frame = getframe(gcf);
     writeVideo(v,frame);
 end
 
 clf;
-
 % Closes the video writer.
 close(v);
-toc %Tracks how long the code runs for
+toc % Tracks how long the code runs for
--- 5D plotting/time_graph.m
+++ 5D plotting/time_graph.m
@@ -1,9 +1,9 @@
 function tGraph = time_graph(tNum, fig_bg_color, word_on, word_off)
-%TIME_GRAPH Bar graph that shows current time of the figure.
-%   Takes an input of the time to be shown, the figure background color,
-%   and the word and off times (for tickm marks)
-bar_graph = barh(tNum);
-        ax = gca;
+    %TIME_GRAPH Bar graph that shows current time of the figure.
+    %   Takes an input of the time to be shown, the figure background color,
+    %   and the word and off times (for tickm marks)
+    bar_graph = barh(tNum);
+    ax = gca;
     xlim([0 44]);
     xticks([word_on word_off]);
     xticklabels({'',''});
@@ -14,11 +14,11 @@ bar_graph = barh(tNum);
     set(gca,'Color',fig_bg_color);
     ylim([0.75 1.25]);
     text(word_on + 0.5,1,'WORD ON','Color',fig_bg_color,'FontSize',24);
-    
+
+    % Changes the color of the bar based on if the word is on or off.
     if tNum < 8 || tNum > 37
         bar_graph.FaceColor = [0.2 0.2 0.2];
     else
         bar_graph.FaceColor = 'red';
     end
 end
-
